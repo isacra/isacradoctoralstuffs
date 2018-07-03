61b85ddc7226f9025de277cb0bd84281aa6ed6c6
isacra <isaac.sacra@gmail.com>
2018-07-03 14:54:09 -0300

    A

--- Apps_And_Libs/scripts_lab/Comparison_Methods/Comparison.m
+++ Apps_And_Libs/scripts_lab/Comparison_Methods/Comparison.m
@@ -1,7 +1,7 @@
 PSF = fspecial('gaussian',2,5);
 V = .0001;
-BlurredNoisy = imnoise(imfilter(y_test_corte_x,PSF),'gaussian',0,V);
+BlurredNoisy = imnoise(imfilter(y_teste_1,PSF),'gaussian',0,V);
 [J P] = deconvblind(BlurredNoisy,PSF,1);
-figure; imagesc(J1)
+%figure; imagesc(J1)
 
-[J1 P1] = deconvblind(x_test_corte_x,PSF,1);
+[J1 P1] = deconvblind(x_teste_1,PSF,1);
--- Apps_And_Libs/scripts_lab/Comparison_Methods/Teste_Wiener_Filter.m
+++ Apps_And_Libs/scripts_lab/Comparison_Methods/Teste_Wiener_Filter.m
@@ -3,7 +3,7 @@ THETA = 15;
 PSF = fspecial('motion', LEN, THETA);
 %PSF = fspecial('log', 9, .5) 
 
-blurred = imfilter(y_teste, PSF, 'conv', 'circular');
+blurred = imfilter(y_teste_1, PSF, 'conv', 'circular');
 figure, imagesc(blurred)
 
 noise_mean = 0;
@@ -13,14 +13,15 @@ blurred_noisy = imnoise(blurred, 'gaussian', ...
 figure, imagesc(blurred_noisy)
 title('Simulate Blur and Noise')
 
-estimated_nsr = 0;
-wnr2 = deconvwnr(blurred_noisy, PSF, estimated_nsr);
-figure, imagesc(wnr2)
-title('Restoration of Blurred, Noisy Image Using NSR = 0')
-
-estimated_nsr = noise_var / var(x_teste(:));
+estimated_nsr = noise_var / var(y_teste_1(:));
 wnr3 = deconvwnr(blurred_noisy, PSF, estimated_nsr);
 figure, imagesc(wnr3)
 title('Saída método Deconvwnr - Wiener Filter');
 %calcfrequencies(y_test_corte_x,x_test_corte_x,wnr3,3);
 
+figure; subplot(1,3,1);imagesc(y_test_corte_x(:,:,2));
+set(gca,'Color','white'); colormap('jet');
+subplot(1,3,2); imagesc(cnnout(:,:,2));
+set(gca,'Color','white'); colormap('jet');
+subplot(1,3,3); imagesc(x_test_corte_x(:,:,2));
+set(gca,'Color','white'); colormap('jet');
